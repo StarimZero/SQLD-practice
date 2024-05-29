47543d321463e7fd5f771eccc2a4fba364b84964
StarimZero <starim-y@naver.com>
2024-05-29 18:23:36 +0900

    book shop CRUD 24.05.29F

--- react.sql
+++ react.sql
@@ -27,6 +27,26 @@ values("orange", "orange", "뜨끈한복치");
 select * from users;
 
 update users set phone = "010-4444-4444", add1 = "서울 관악구 신림로29길 8", add2="신림현대아파트 107동" where uid= "green";
+update users set photo = null where uid = "red";
 
+
+create table books (
+	bid int auto_increment primary key, 
+    title varchar(500) not null,
+    price int default 0,
+    contents text,
+    isbn varchar(100),
+    publisher varchar(100),
+    image varchar(200),
+    author varchar(200),
+    regDate datetime default now()
+);
+desc books;
+drop table books;
+
+select * from books;
+select count(*) from books;
+delete from books where bid="109";
+select *, date_format(regDate, "%Y-%m-%d %H시-%i분-%s초 %d일") fmtdate, format(price,0) fmtprice from books order by bid desc limit 0,5;
 
 
